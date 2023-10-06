180f33f5db720c331849c360cf2af519c291c621
João Knisspell <99961057+joaoKnisspell@users.noreply.github.com>
2023-10-06 13:35:09 -0300

    Fixing form layout

--- src/components/FormPage/index.tsx
+++ src/components/FormPage/index.tsx
@@ -6,8 +6,8 @@ type FormPageProps = ComponentProps<'div'> & {
 
 export function FormPageWrapper({ title, ...props }: FormPageProps) {
   return (
-    <div className="flex h-full w-full flex-col items-center justify-center">
-      <div className="flex w-96 flex-col">
+    <div className="flex h-full min-h-screen w-full flex-col items-center justify-center lg:min-h-0">
+      <div className="flex w-[300px] flex-col py-20 md:w-96 md:py-0">
         <h3 className="text-2xl text-appGray500">{title}</h3>
         {props.children}
       </div>
